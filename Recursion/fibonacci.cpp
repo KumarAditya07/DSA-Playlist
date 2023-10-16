6e9b0477638c68b563532bf006b7d3f82c8093a7
Kumar Aditya <122738572+KumarAditya07@users.noreply.github.com>
2023-10-16 14:20:39 +0000

    fibonacii using recursion

---
+++ Recursion/fibonacci.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int fibonacci(int n) {
+      if(n<=1) return n;
+      return fibonacci(n-1)+fibonacci(n-2);
+
+}
+
+int main() {
+ int n ;
+ cout<<"Enter the Number : ";
+ cin>>n;
+  int ans = fibonacci(n);
+  cout<<ans;
+  
+}
